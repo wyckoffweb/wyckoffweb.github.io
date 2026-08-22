1250b0827f1612481b2b953e263ce0483e2bb926
wyckoffweb <fr.parakkal@gmail.com>
2026-08-22 13:29:30 +0530

    Deploy latest Wyckoff terminal UI update

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'wyckoffweb-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/learn.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).catch(() => cachedResponse);
+    })
+  );
+});
